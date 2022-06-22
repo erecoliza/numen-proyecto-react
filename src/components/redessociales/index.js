b76c50fc57cb81617a12ff686ef896bc86a8934b
erecoliza <eduardo.recoliza@gmail.com>
2022-06-22 16:18:19 -0300

    nuevas imagenes

--- src/components/redessociales/index.js
+++ src/components/redessociales/index.js
@@ -1,9 +1,9 @@
-import imgFacebook from '../../assets/facebook.png';
-import imgGorjeo from '../../assets/gorjeo.png';
-import imgInstagram from '../../assets/instagram.png';
-import imgMensajero from '../../assets/mensajero.png';
-import imgTelegram from '../../assets/telegram.png';
-import imgWhatsapp from '../../assets/whatsapp.png';
+import imgFacebook from '../../assets/redessociales/facebook.png';
+import imgGorjeo from '../../assets/redessociales/gorjeo.png';
+import imgInstagram from '../../assets/redessociales/instagram.png';
+import imgMensajero from '../../assets/redessociales/mensajero.png';
+import imgTelegram from '../../assets/redessociales/telegram.png';
+import imgWhatsapp from '../../assets/redessociales/whatsapp.png';
 import { Div, Image } from './redessociales.styled';
 
 const RedesSociales = () => {
